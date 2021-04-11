0494b7fd7354e2620ea6697e0e16a33e444121ca
gregori0o <glegeza@wp.pl>
2021-04-11 23:31:00 +0200

    twisted client

--- android_app/main.py
+++ android_app/main.py
@@ -1,3 +1,37 @@
+from __future__ import unicode_literals
+
+from kivy.support import install_twisted_reactor
+
+install_twisted_reactor()
+
+# A Simple Client that send messages to the Echo Server
+from twisted.internet import reactor, protocol
+
+
+class EchoClient(protocol.Protocol):
+    def connectionMade(self):
+        self.factory.app.on_connection(self.transport)
+
+    def dataReceived(self, data):
+        self.factory.app.print_message(data.decode('utf-8'))
+
+
+class EchoClientFactory(protocol.ClientFactory):
+    protocol = EchoClient
+
+    def __init__(self, app):
+        self.app = app
+
+    def startedConnecting(self, connector):
+        self.app.print_message('Started to connect.')
+
+    def clientConnectionLost(self, connector, reason):
+        self.app.print_message('Lost connection.')
+
+    def clientConnectionFailed(self, connector, reason):
+        self.app.print_message('Connection failed.')
+
+
 from kivy.app import App
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.button import Button
@@ -5,18 +39,36 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.label import Label
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy_garden.zbarcam import ZBarCam
-import socket as sck
+from time import sleep
+
 
 class MainApp(App):
+    connection = None
+    textbox = None
+    label = ""
+
     def build(self):
         screenmanager = ScreenManager()
         screen_start = FirstStep(name='start')
-        screen_QR = QRreader(name='QR')
-        screen_main = MainScreen(name='main')
+        screen_QR = QRreader(self, name='QR')
+        screen_main = MainScreen(self, name='main')
         screenmanager.add_widget(screen_start)
         screenmanager.add_widget(screen_QR)
         screenmanager.add_widget(screen_main)
         return screenmanager
+
+    def connect_to_server(self, ip, port):
+        reactor.connectTCP(ip, port, EchoClientFactory(self))
+
+    def on_connection(self, connection):
+        self.print_message("Connected successfully!")
+        self.connection = connection
+
+    def print_message(self, msg):
+        pass
+        #self.label += "{}\n".format(msg)
+        #print(self.label)
+
        
 class FirstStep (Screen):
     def __init__(self, **kwargs):
@@ -46,8 +98,9 @@ class FirstStep (Screen):
 
 
 class QRreader (Screen):
-    def __init__(self, **kwargs):
+    def __init__(self, app, **kwargs):
         super (QRreader, self).__init__(**kwargs)
+        self.app = app
 
         QR_layout = FloatLayout()
         label = Label(text="Take photo of QR code or enter data",
@@ -88,28 +141,26 @@ class QRreader (Screen):
         data = self.read_text.text
 
         ip = data.split(',')[0]
-        port = data.split(',')[1]        
+        port = data.split(',')[1]
+        #ip = "192.168.100.89"
+        #port = 8000  
         
-        client_socket = sck.socket()
-        client_socket.connect((ip, int(port))) 
-
-        client_socket.send('OMG'.encode())
-        
-        if not data:
-            self.read_text.text = "Connection failed. Enter new data."
-        else:
-            self.zbarcam.stop()
-            self.manager.current = 'main'
+        self.app.connect_to_server(ip, port)
+        self.manager.current = 'main'
+        #if not self.app.connection:
+        #    self.read_text.text = "Connection failed. Enter new data."
+        #else:
+        #    #self.zbarcam.stop()
+        #    self.manager.current = 'main'
 
     def make_photo (self, *args):
-        pass
+        self.read_text.text = ', '.join([str(symbol.data) for symbol in self.zbarcam.symbols])
 
-    def repeat (self, *args):
-        pass
 
 class MainScreen (Screen):
-    def __init__(self, **kwargs):
+    def __init__(self, app, **kwargs):
         super (MainScreen, self).__init__(**kwargs)
+        self.app = app
 
         main_layout = FloatLayout()
         label = Label(text="Enter command for computer.",
@@ -136,14 +187,14 @@ class MainScreen (Screen):
         self.add_widget(main_layout)
 
     def execute (self, *args):
-        command = self.read_text.text
+        msg = self.read_text.text
 
-        #execute command
-
-        if not command:
-            self.read_text.text = "No, some problem"
-        else:
+       
+        if msg and self.app.connection:
+            self.app.connection.write(msg.encode('utf-8'))
             self.read_text.text = "Nice"
+        else:
+            self.read_text.text = "Some problem"
 
 if __name__ == "__main__":
     app = MainApp()
