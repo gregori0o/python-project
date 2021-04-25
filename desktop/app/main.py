f904b5ed0690d0f25e58605300b21d8a5f042232
Kacper Kafara <kacperkafara@gmail.com>
2021-04-25 14:24:35 +0200

    Renaming

--- desktop/app/main.py
+++ desktop/app/main.py
@@ -10,11 +10,9 @@ from kivy.uix.image import Image
 import os
 import qrcode
 
-from server.twistedserver import *
+from server.server import Server
 
 DEFAULT_PORT = 8000
-
-
 
 class QRCodeButton(Button):
     state = BooleanProperty(True)
@@ -31,19 +29,24 @@ class QRCodeButton(Button):
             return True
         return super().on_touch_down(touch)
 
-    def on_state(self, instance, pos):
-        pass
-
         
 
 
-class Application(App):
+class DesktopApp(App):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # spliting in case device is connected by more than one interface
+        # self.ipv4_server_address = os.popen('hostname -I').read().split(' ')[0]
+        self.server = Server(self, )
+        self.server.listen()
+    
+    
     def build(self):
         self.screen_manager = ScreenManager()
         
-        self.screens = [Screen(name='main'), Screen(name='qrcode')]
-        self.screen_manager.add_widget(self.screens[0])
-        self.screen_manager.add_widget(self.screens[1])
+        self.screens = {'main': Screen(name='main'), 'qrcode': Screen(name='qrcode')}
+        self.screen_manager.add_widget(self.screens['main'])
+        self.screen_manager.add_widget(self.screens['qrcode'])
 
         
         self.main_layout = BoxLayout()
@@ -51,40 +54,31 @@ class Application(App):
         self.qrcode_button.bind(state=self.show_qrcode)
         self.main_layout.add_widget(self.qrcode_button)
  
-        self.screens[0].add_widget(self.main_layout)
+        self.screens['main'].add_widget(self.main_layout)
 
         self.qrcode_layout = BoxLayout()
         # self.qrcode_layout.add_widget(Image(source='qrcode.png'))
-        self.screens[1].add_widget(self.qrcode_layout) 
-        
-        reactor.listenTCP(DEFAULT_PORT, ServerFactory(self))
-        
-        self.ipv4_server_address = os.popen('hostname -I').read()
-
+        self.screens['qrcode'].add_widget(self.qrcode_layout) 
         
         return self.screen_manager
-    
+        
 
     def handle_message(self, data: bytes):
         print(data.decode('utf-8'))
-        
+
     
     def show_qrcode(self, instance, pos):
         if len(self.qrcode_layout.children) == 0:
             qrcode_img = qrcode.make(f"{self.ipv4_server_address},{DEFAULT_PORT}")
-            qrcode_img.save('qrcode.png', format='png')
-            self.qrcode_layout.add_widget(Image(source='qrcode.png'))
-        self.screen_manager.switch_to(self.screens[1])
+            qrcode_img.save('qrcode/qrcode.png', format='png')
+            self.qrcode_layout.add_widget(Image(source='qrcode/qrcode.png'))
+        self.screen_manager.switch_to(self.screens['qrcode'])
+    
+    
 
-    
-    
-    
     
 def main():
-    Application().run()
+    DesktopApp().run()
 
-
-    
 if __name__ == '__main__':
     main()
-    
