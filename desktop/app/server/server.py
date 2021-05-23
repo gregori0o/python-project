3eaa8f1fc85990d14ea0fee0d4019fcf08537c0e
Kacper Kafara <kacperkafara@gmail.com>
2021-05-23 21:15:32 +0200

    Added automatic searching for a free port

--- desktop/app/server/server.py
+++ desktop/app/server/server.py
@@ -5,7 +5,7 @@ from collections import namedtuple
 
 from kivy.support import install_twisted_reactor
 
-# documentations says that install_twisted_reactor must be called 
+# documentation says that install_twisted_reactor must be called 
 # before importing reactor from twisted.internet
 install_twisted_reactor()
 
@@ -13,27 +13,46 @@ from twisted.internet import reactor
 from twisted.internet.endpoints import TCP4ServerEndpoint
 from twisted.internet.protocol import Factory, Protocol
 from server.protocol import AppProtocol
+import socket
 import os
 
 
 class Server(Factory):
-    DEFAULT_PORT: Literal = 8000
-    
+
+    DEFAULT_PORT: Literal[4000] = 4000
+
+
     def __init__(self, app):
         self.app = app
         self.client = None
         
         # spliting in case device is connected on more than one interface
+        # this code is OS specific
         self.ip = os.popen('hostname -I').read().split(' ')[0]
-        self.port = Server.DEFAULT_PORT
-        
+
+        # may throw IOError exception
+        self.port = self.find_free_port(Server.DEFAULT_PORT)
+
+
+    def find_free_port(self, port: int = 4000, max_port: int = 60_000):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        while port <= max_port:
+            try:
+                sock.bind(('', port))
+                sock.close()
+                return port
+            except OSError:
+                port += 1
+        raise IOError('No free port available')
+    
 
     def buildProtocol(self, addr: tuple[str, int]) -> "Protocol":
+            print('Server.buildProtocol:', addr)
             return AppProtocol(self)
-
+            
     
     def run(self):
-        endpoint = TCP4ServerEndpoint(reactor, Server.DEFAULT_PORT, interface='')
+        endpoint = TCP4ServerEndpoint(reactor, self.port, interface='')
         endpoint.listen(self)
         # reactor.run()
     
